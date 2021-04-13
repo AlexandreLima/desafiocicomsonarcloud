d715037070c9452c4c996d8d675137d3f10abf3f
AlexandreLima <xandebre@gmail.com>
2021-04-12 23:02:15 -0300

    feat: add sonar to tests

---
+++ sonar-project.js
@@ -0,0 +1,16 @@
+const sonarqubeScanner =  require('sonarqube-scanner');
+sonarqubeScanner(
+    {
+        serverUrl:  'http://localhost:9000',
+        options : {
+            'sonar.sources':  'src',
+            'sonar.tests':  'src',
+            'sonar.inclusions'  :  '**', // Entry point of your code
+            'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+            'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
+            'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml',
+            'sonar.login' :'1199650bac245dab134a2282a0a3c338652d3147',
+            'sonar.projectKey' : 'node-teste',
+            'sonar.projectName' : 'node-teste'
+        }
+    }, () => {});
